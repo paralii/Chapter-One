5f92fa196e4bb1e5997f59853683cb62339fcfcb
paralii <sabeehsadath14@gmail.com>
2025-08-01 09:08:19 +0530

    feat: force logout blocked users with toast and redirect

--- Frontend/src/api/userAxios.js
+++ Frontend/src/api/userAxios.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { toast } from "react-toastify";
+import { logoutUser } from "../redux/authSlice";
+import store from "../redux/store.js";
 
 const userAxios = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/user`,
@@ -10,8 +13,14 @@ userAxios.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
+    if(error.response?.status === 403 && error.response.data.message === "Blocked account") {
+      store.dispatch(logoutUser());
+      toast.error("Your account has been blocked by admin");
+      window.location.href = "/login";
+      return Promise.reject(error);
+    }
     if (
-      (error.response.status === 401 || error.response.status === 403) &&
+      (error.response?.status === 401) &&
       !originalRequest._retry
     ) {
       originalRequest._retry = true;
@@ -23,7 +32,9 @@ userAxios.interceptors.response.use(
         );
         return userAxios(originalRequest);
       } catch (refreshError) {
+        setTimeout(() => {
         window.location.href = "/login";
+      }, 1000);
         return Promise.reject(refreshError);
       }
     }
