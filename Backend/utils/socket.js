a48a878f819061a5cf899ff38308203eb66d127c
paralii <sabeehsadath14@gmail.com>
2025-09-11 17:55:02 +0530

    socket.js

--- Backend/utils/socket.js
+++ Backend/utils/socket.js
@@ -7,7 +7,13 @@ const userSockets = new Map();
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: process.env.CORS_URL,
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
       credentials: true,
     },
   });
